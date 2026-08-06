8aa224aa9864a905cf0582b325412c695cc67568
mahiro31 <mk2.mahiro@gmail.com>
2026-08-07 03:19:44 +0900

    サンプルの準備

--- src/app/sample2/sample2.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-sample2',
-  imports: [],
-  templateUrl: './sample2.html',
-  styleUrl: './sample2.css',
-})
-export class Sample2 {}
