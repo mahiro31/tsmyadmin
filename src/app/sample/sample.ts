9d7f845f71453520aa88b9c17c52411420ab16e5
mahiro31 <mk2.mahiro@gmail.com>
2026-08-07 01:17:56 +0900

    サンプル作成

---
+++ src/app/sample/sample.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sample',
+  imports: [],
+  templateUrl: './sample.html',
+  styleUrl: './sample.css',
+})
+export class Sample {}
