5d8cbf483d8a6df6c0f77fc9ff1de2f4f6909c40
mahiro31 <mk2.mahiro@gmail.com>
2026-08-07 02:26:43 +0900

    一旦ルーティングをリセット

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,3 @@
 import { Routes } from '@angular/router';
-import { Sample } from './sample/sample';
 
-export const routes: Routes = [
-    {path: 'sample', component: Sample},
-];
+export const routes: Routes = [];
