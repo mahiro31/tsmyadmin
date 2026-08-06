e29517eead55d665f5c7d5aedfa18356b59ea92a
mahiro31 <mk2.mahiro@gmail.com>
2026-08-07 03:48:54 +0900

    デフォルトのルートページを削除

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
 import { ExsampleComponent } from './exsample/exsample.component';
+import { Sample } from './sample/sample';
 
 export const routes: Routes = [
+  { path: '', component: App },
+  { path: 'sample', component: Sample },
   { path: 'exsample', component: ExsampleComponent },
 ];
